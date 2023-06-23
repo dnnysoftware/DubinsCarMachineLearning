705419e2c90d8ce97235bd42658c2e788c20e34c
Daniel Mironiuk <softwarebydanielmironiuk@gmail.com>
2023-06-23 11:05:07 -0400

    Implemented euleurs method to find costof current position to desired final position

---
+++ src/ode.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+def is_feasible(s):
+    # boundary conditions for trajectory planning
+    if (s[0] <= -4 and s[1] > 3) or ((s[0] > -4 and s[0] < 4) and s[1] > -1) or (s[0] >= 4 and s[1] > 3):
+        return True
+    return False
+
+
+def f(sk, uk):
+    # Uses Eulers method to derive the next state
+    # sk = [xk yk ak vk], uk = [yk Bk]
+    xk_next = sk[3]* np.cos(sk[2])
+    yk_next = sk[3]* np.sin(sk[2])
+    ak_next = uk[0]
+    vk_next = uk[1]
+    sk_next = np.array([xk_next, yk_next, ak_next, vk_next])
+    return sk_next
+
+
+def interpolate_individual(i, num_points):
+    # Interpolates how many interpolated points betweem optimization parameters
+    indices = np.arange(i.shape[0])
+    new_indices = np.linspace(0, i.shape[0]-1, num_points*(i.shape[0]-1)+i.shape[0])
+    interpolated_i = np.zeros((len(new_indices), i.shape[1]))
+    for j in range(i.shape[1]):
+        interpolated_i[:,j] = np.interp(new_indices, indices, i[:,j])
+    return interpolated_i
+
+
+def calculate_cost(sf, sf_prime, config):
+    # sf_prime is required final state, sf is derived final state, K is infeasible constant
+    if is_feasible(sf):
+        return calculate_euclidean_distance(sf, sf_prime)
+    else:
+        return config['ODE']['K']
+    
+
+def calculate_euclidean_distance(sf, sf_prime):
+    # calculates the cost (J) with feasible boundary conditions
+    return ((sf_prime[0] - sf[0])**2 
+            + (sf_prime[1] - sf[1])**2 
+            + (sf_prime[2] - sf[2])**2 
+            + (sf_prime[3] - sf[3])**2)**.5
+
+
+
+def calculate_eulers(T, dt, s0, individual):
+    s = np.zeros((int(T / dt) + 1, 4))
+    s[0, :] = s0
+    t = np.zeros((int(T / dt) + 1,))
+    t[0] = 0
+    feasible = True
+    for i in range(1, int(T / dt) + 1):
+        k = f(s[i-1, :], individual[i - 1])
+        s[i, :] = s[i-1, :] + k * dt
+        t[i] = t[i-1] + dt
+        if is_feasible(s[i]) == False:
+            feasible = False
+    return s, t, feasible
--- src/parking.py
+++ src/parking.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+import ode
 
 def calculate_time_passed(start_time):
     # Calculates time passed from start of reinforcement process
@@ -40,10 +41,20 @@ def convert_binary_population_to_decimal(population, config):
     return dec_population
 
 
+def determine_cost_for_individual(individual, s0, sf_prime, config):
+    num_points = 1 / config['ODE']['DT'] # the number of points between parameters each original parameter is 1 second between others
+    interpolated_individual = ode.interpolate_individual(individual, int(num_points))
+    states, times, feasible = ode.calculate_eulers(config['ODE']['T'], config['ODE']['DT'], s0, interpolated_individual)
+    if feasible:
+        return calculate_cost(states[len(states) - 1], sf_prime, config), states, times, interpolated_individual
+    return config['ODE']['K'], states, times, interpolated_individual
+
+
 def genetic_algorithm(population, start_time, s0, sf_prime, plot, config):
     # Repeat until a fit enough individual is found or enough time has elapsed
     generation = 0
     while True and calculate_time_passed(start_time) < config['CONSTRAINTS']['MAX_EXEC_TIME_SEC'] and \
         generation <= config['CONSTRAINTS']['MAX_NUM_GEN']:
         dec_pop = convert_binary_population_to_decimal(population, config)
+        costs = [determine_cost_for_individual(p, s0, sf_prime) for p in dec_pop]
         generation += 1
